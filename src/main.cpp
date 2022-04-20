4f4695d76d67dd3d1bb381a5c1eab8e99333717d
gellidao <gellidao@gmail.com>
2022-04-20 18:43:24 -0500

    It connects to internet and display a server with a React App

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,40 @@
 #include "SPIFFS.h"
 #include <ESPAsyncWebServer.h>
 
-const char* ssid     = "ESP32-Access-Point";
-const char* password = "123456789";
+/*const char* ssid     = "ESP32-Access-Point";
+const char* password = "123456789"; */
+
+const char* ssid = "IZZI-2124";
+const char* password =  "Antigatos7";
+  
+
+AsyncWebServer server(80);
 
 void setup() {
-  WiFi.softAP(ssid, password);
+  Serial.begin(115200);
+  //WiFi.softAP(ssid, password);
+  if(!SPIFFS.begin()){
+        while(true) ; 
+  }
+
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  
+  Serial.println(WiFi.localIP());
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+
+  server.on("/app.js", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/app.js", "text/javascript");
+  });
+
+  server.begin();
 }
 
 void loop() {
