11d5138b36543f2ca7b79ec91402ce72df256b05
gellidao <gellidao@gmail.com>
2022-04-21 23:59:55 -0500

    React and SASS preprocesor is loaded from web, same than bboostrap, also a DNS server is running, and app was replaced by a make by me app

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
 #include <Arduino.h>
 #include <Servo.h>
 #include <WiFi.h>
+#include <ESPmDNS.h> // Call the server with name istead of IP
 #include "SPIFFS.h"
 #include <ESPAsyncWebServer.h>
 
-/*const char* ssid     = "ESP32-Access-Point";
-const char* password = "123456789"; */
 
 const char* ssid = "IZZI-2124";
 const char* password =  "Antigatos7";
@@ -15,19 +14,26 @@ AsyncWebServer server(80);
 
 void setup() {
   Serial.begin(115200);
-  //WiFi.softAP(ssid, password);
+
+  //Begin FileSystem
   if(!SPIFFS.begin()){
         while(true) ; 
   }
 
-  WiFi.begin(ssid, password);
-  
+  // Begin Wifi Conection 
+  WiFi.begin(ssid, password);  
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
-  
   Serial.println(WiFi.localIP());
+
+  // Begin DNS server to find device: http://esp32.local
+  if(!MDNS.begin("esp32")) {
+     Serial.println("Error starting mDNS");
+     return;
+  }
+
 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(SPIFFS, "/index.html", "text/html");
@@ -35,6 +41,14 @@ void setup() {
 
   server.on("/app.js", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(SPIFFS, "/app.js", "text/javascript");
+  });
+
+  server.on("/styles.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/styles.css", "text/javascript");
+  });
+
+  server.on("/hello", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain", "Hello World");
   });
 
   server.begin();
