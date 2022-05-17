01434960c92cba98869d1f88127dcb51392ded6e
gellidao <gellidao@gmail.com>
2022-05-17 13:07:44 -0500

    Web app sends information and this is storage in a class called state, also the pwm to the actuators is updated, this will be then used when websocket comunication with pc program, which is the next task

--- data/app.js
+++ data/app.js
@@ -2,42 +2,72 @@ class App extends React.Component{
   constructor(props){
     super(props) ; 
     this.state = {
+      motor0: 0, 
       motor1: 0, 
-      motor2: 0, 
+      servo0: 0, 
       servo1: 0, 
-      servo2: 0, 
-      servo3: 0
+      servo2: 0
     }
-    this.updateMotor1 = this.updateMotor1.bind(this) ; 
-    this.updateMotor2 = this.updateMotor2.bind(this) ;
+    this.updateMotor0 = this.updateMotor0.bind(this) ; 
+    this.updateMotor1 = this.updateMotor1.bind(this) ;
+    this.updateServo0 = this.updateServo0.bind(this) ;
     this.updateServo1 = this.updateServo1.bind(this) ;
-    this.sendData = this.sendData.bind(this); 
+    this.updateServo2 = this.updateServo2.bind(this) ;
+    this.sendServo0Data = this.sendServo0Data.bind(this); 
+    this.sendServo1Data = this.sendServo1Data.bind(this); 
+    this.sendServo2Data = this.sendServo2Data.bind(this); 
+    this.sendMotor0Data = this.sendMotor0Data.bind(this); 
+    this.sendMotor1Data = this.sendMotor1Data.bind(this); 
   }
   
-  updateMotor1(e){
+  updateMotor0(e){
+    this.setState({
+      motor0: e.target.value,
+    }, ()=>{this.sendMotor0Data()})
+  }
+  
+   updateMotor1(e){
     this.setState({
       motor1: e.target.value,
-    })
-  }
-  
-   updateMotor2(e){
-    this.setState({
-      motor2: e.target.value,
-    })
+    }, ()=>{this.sendMotor1Data()})
    }
      
-    updateServo1(e){
+    updateServo0(e){
     this.setState({
-      servo1: e.target.value,
-    }, ()=>{this.sendData()})
+      servo0: e.target.value,
+    }, ()=>{this.sendServo0Data()})
+    }
+
+    updateServo1(e){
+      this.setState({
+        servo1: e.target.value,
+      }, ()=>{this.sendServo1Data()})
+      }
+
+      updateServo2(e){
+        this.setState({
+          servo2: e.target.value,
+        }, ()=>{this.sendServo2Data()})
+        }
+
+    sendMotor0Data(){
+      fetch('/senData?motor0='+this.state.motor0)
+    }
+
+    sendMotor1Data(){
+      fetch('/senData?motor1='+this.state.motor1)
     }
   
-    sendData(){
+    sendServo0Data(){
+      fetch('/senData?servo0='+this.state.servo0)
+    }
+
+    sendServo1Data(){
       fetch('/senData?servo1='+this.state.servo1)
-      //.then(response => response.json())
-      //.then(data => {
-      //  document.getElementById('message').innerHTML = JSON.stringify(data);
-      //})
+    }
+
+    sendServo2Data(){
+      fetch('/senData?servo2='+this.state.servo2)
     }
   
   render(){
@@ -45,40 +75,64 @@ class App extends React.Component{
       <div id="mainContainer"> 
           <h1> Remote control of motors and servomotors </h1>
         
+        <p className="actuator">Motor 0: </p>
+        <div id="bottonZoneMotor0" className="bottonZone">
+          <input className="bars" type="range"
+          value={this.state.motor0}
+          min="0"
+          max="200"
+          step="1"
+          onChange={this.updateMotor0}
+          />
+          <div className="output">{this.state.motor0}</div>
+        </div>
+        
         <p className="actuator">Motor 1: </p>
-        <div id="bottonZoneMotor1" className="bottonZone">
+        <div id="bottonZoneMotor1" className="bottonZone" >
           <input className="bars" type="range"
           value={this.state.motor1}
           min="0"
-          max="255"
+          max="200"
           step="1"
           onChange={this.updateMotor1}
           />
           <div className="output">{this.state.motor1}</div>
         </div>
         
-        <p className="actuator">Motor 2: </p>
-        <div id="bottonZoneMotor2" className="bottonZone" >
+        <p className="actuator">Servo 0: </p>
+        <div id="bottonZoneServo0" className="bottonZone" >
           <input className="bars" type="range"
-          value={this.state.motor2}
-          min="0"
-          max="255"
+          value={this.state.servo0}
+          min="-30"
+          max="30"
           step="1"
-          onChange={this.updateMotor2}
+          onChange={this.updateServo0}
           />
-          <div className="output">{this.state.motor2}</div>
+          <div className="output">{this.state.servo0}</div>
         </div>
-        
+
         <p className="actuator">Servo 1: </p>
         <div id="bottonZoneServo1" className="bottonZone" >
           <input className="bars" type="range"
           value={this.state.servo1}
-          min="0"
-          max="180"
+          min="-30"
+          max="30"
           step="1"
           onChange={this.updateServo1}
           />
           <div className="output">{this.state.servo1}</div>
+        </div>
+
+        <p className="actuator">Servo 2: </p>
+        <div id="bottonZoneServo2" className="bottonZone" >
+          <input className="bars" type="range"
+          value={this.state.servo2}
+          min="-30"
+          max="30"
+          step="1"
+          onChange={this.updateServo2}
+          />
+          <div className="output">{this.state.servo2}</div>
         </div>
       </div>
     ) ; 
--- src/main.cpp
+++ src/main.cpp
@@ -7,36 +7,76 @@
 #include <string>
 
 
-const char* ssid = "FIME-POSGRADO";
-const char* password =  "pereira2018";
+//const char* ssid = "FIME-POSGRADO";
+//const char* password =  "pereira2018";
+const char* ssid = "IZZI-2124";
+const char* password =  "Antigatos7";
 
-const char* input_parameter1 = "input_string";
-const char* input_parameter2 = "input_integer";
-const char* input_parameter3 = "input_float";
   
-Servo servoPin2 ; //M8
+Servo actuator[5] ; 
+int pines[5] = {27, 25, 2, 26, 0}; // M5, M7, M8, M6, ECO ULTRA
 
 AsyncWebServer server(80);
 
-/*char strin2int(String str){
-  char output; 
-  if(sizeof(str) == 3){
-    output = 100 * *((char *) str[0]) ; 
-    output += 10 * *((char *) str[1]) ;
-    output += *((char *) str[2]) ;
-  }  else if(str.len() == 2){
-    output = 10 * *((char *) str[1]) ;
-    output += *((char *) str[2]) ;
-  } else if(str.len() == 0){
-    output = *((char *) str[2]) ; 
-  }
-  return output ; 
-}*/
+class State {
+  public: 
+    char motor0, motor1, servo0, servo1, servo2 ; 
+    State() ; 
+    void setMotor0(int) ; 
+    void setMotor1(int) ; 
+    void setServo0(int) ; 
+    void setServo1(int) ; 
+    void setServo2(int) ;  
+} ;
+
+State::State(){
+  motor0 = 0;
+  motor1 = 0; 
+  servo0 = 0; 
+  servo1 = 0; 
+  servo2 = 0;  
+}; 
+
+void State::setMotor0(int pwm){
+  pwm = pwm*180/255 ; // It is mapped to scale [0, 180]
+  motor0 = (char) pwm ; 
+  actuator[0].write(pwm) ;
+} ; 
+
+void State::setMotor1(int pwm){
+  pwm = pwm*180/255 ; // It is mapped to scale [0, 180]
+  motor1 = (char) pwm ; 
+  actuator[1].write(pwm) ;
+} ; 
+
+void State::setServo0(int angle){
+  angle = angle + 90 ; // If angle goes from -30 to 30 degrees
+  servo0 = (char) angle ; 
+  actuator[2].write(angle) ;
+} ; 
+
+void State::setServo1(int angle){
+  angle = angle + 90 ; // If angle goes from -30 to 30 degrees
+  servo1 = (char) angle ; 
+  actuator[3].write(angle) ;
+} ; 
+
+void State::setServo2(int angle){
+  angle = angle + 90 ; // If angle goes from -30 to 30 degrees
+  servo2 = (char) angle ; 
+  actuator[4].write(angle) ;
+} ; 
+
+State state ; 
 
 void setup() {
-  Serial.begin(115200);
 
-  servoPin2.attach(2) ;
+  for(int i=0; i < 5; i++){
+    actuator[i].attach(pines[i]) ; 
+  }
+
+  
+  Serial.begin(115200);
 
   //Begin FileSystem
   if(!SPIFFS.begin()){
@@ -75,25 +115,32 @@ void setup() {
   });
 
   server.on("/senData", HTTP_GET, [](AsyncWebServerRequest *request){
-    int paramsNr = request->params();
-    Serial.println(paramsNr);
- 
-    for(int i=0;i<paramsNr;i++){
- 
-        AsyncWebParameter* p = request->getParam(i);
-        Serial.print("Param name: ");
-        Serial.println(p->name());
-        Serial.print("Param value: ");
-        Serial.println(p->value()); 
-        Serial.println("------");
-        /*char * servo1Input ; 
-        i<p->value()
-        for(int i=0; i<p->value().length(); i++){
-
-        }
-        strcpy(servo1Input, p->value())  ; 
-        servoPin2.write(atoi(servo1Input));*/
-        //servoPin2.write(atol(servo1Input));
+    //int paramsNr = request->params(); // return the number of parameters
+    AsyncWebParameter* p = request->getParam(0);
+    if(p->name() == "motor0"){
+        state.setMotor0((p->value()).toInt()) ; 
+        //Serial.print("motor0: ") ; 
+        //Serial.println((int) state.motor0) ; 
+    }
+    if(p->name() == "motor1"){
+        state.setMotor1((p->value()).toInt()) ; 
+        //Serial.print("motor1: ") ; 
+        //Serial.println((int) state.motor1) ; 
+    }
+    if(p->name() == "servo0"){
+        state.setServo0((p->value()).toInt()) ; 
+        //Serial.print("servo0: ") ; 
+        //Serial.println((int) state.servo0) ; 
+    }
+    if(p->name() == "servo1"){
+        state.setServo1((p->value()).toInt()) ; 
+        //Serial.print("servo1: ") ; 
+        //Serial.println((int) state.servo1) ; 
+    }
+    if(p->name() == "servo2"){
+        state.setServo2((p->value()).toInt()) ; 
+        //Serial.print("servo2: ") ; 
+        //Serial.println((int) state.servo2) ; 
     }
     request->send(200, "text/plain", "message received");
   });
